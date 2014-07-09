be3702f4cfdf71824f389e28a52e06192e9e025f
markxueyuan <ziyaajiang@gmail.com>
2014-07-09 16:41:08 +0800

    more on chapter4

--- chapter4.lisp
+++ chapter4.lisp
@@ -8,3 +8,93 @@
 (let ((n (find-if #'(lambda (x) (> x 2)) '(1 2 3 4))))
 	(values n ((lambda (x) (* x 2)) n)))
 
+(defun find2
+	(fn lst)
+	(if (null lst)
+		nil
+		(let ((val (funcall fn (car lst))))
+			(if val
+				(values (car lst) val)
+				(find2 fn (cdr lst))))))
+
+
+(proclaim '(inline last1 single append1 conoc1 mklist))
+
+
+(defun last1 (lst)
+	(car (last lst)))
+
+(defun single (lst)
+	(and (consp lst) (not (cdr lst))))
+
+(defun append1 (lst obj)
+	(append lst (list obj)))
+
+(defun conc1 (lst obj)
+	(nconc lst (list obj)))
+
+(defun mklist (obj)
+	(if (listp obj) obj (list obj)))
+
+(defun longer (x y)
+	(labels ((compare (x y)
+				(and (consp x)
+					(or (null y)
+						(compare (cdr x) (cdr y))))))
+		(if (and (listp x) (listp y))
+			(compare x y)
+			(> (length x) (length y)))))
+
+(defun filter (fn lst)
+	(let ((acc nil))
+		(dolist (x lst)
+			(let ((val (funcall fn x)))
+				(if val (push val acc))))
+	(nreverse acc)))
+
+(defun group (source n)
+	(if (zerop n) (error "zero length"))
+	(labels ((rec (source acc)
+				(let ((rest (nthcdr n source)))
+					(if (consp rest)
+						(rec rest (cons (subseq source 0 n) acc))
+						(nreverse (cons source acc))))))
+		(if source (rec source nil) nil)))
+
+(defun flatten (x)
+	(labels ((rec (x acc)
+				(cond ((null x) acc)
+		              ((atom x) (cons x acc))
+		              (t (rec (car x) (rec (cdr x) acc))))))
+	(rec x nil)))
+
+(defun prune (test tree)
+	(labels ((rec (tree acc)
+				(cond ((null tree) (nreverse acc))
+					  ((consp (car tree)) (rec (cdr tree) (cons (rec (car tree) nil) acc)))
+					  (t (rec (cdr tree) (if (funcall test (car tree))
+											 acc
+											 (cons (car tree) acc)))))))
+		(rec tree nil)))
+
+(defun before (x y lst &key (test #'eql))
+	(and lst
+		(let ((first (car lst)))
+			(cond ((funcall test y first) nil)
+				  ((funcall test x first) lst)
+				  (t (before x y (cdr lst) :test test))))))
+
+(defun after (x y lst &key (test #'eql))
+	(let ((rest (before y x lst :test test)))
+		(and rest (member x rest :test test))))
+
+(defun duplicate (obj lst &key (test #'eql))
+	(member obj (cdr (member obj lst :test test)) :test test))
+
+(defun split-if (fn lst)
+	(let ((acc nil))
+		(do ((src lst (cdr src)))
+			((or (null src) (funcall fn (car src)))
+			 (values (nreverse acc) src))
+		  (push (car src) acc))))
+	
